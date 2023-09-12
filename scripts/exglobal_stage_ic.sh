99cd9bcb8eb1fb2e332058740f4b135168b7f3d8
AnilKumar-NOAA <anil.kumar@noaa.gov>
2023-09-12 16:25:29 +0000

    start staging ics for gefs

--- scripts/exglobal_stage_ic.sh
+++ scripts/exglobal_stage_ic.sh
@@ -16,7 +16,7 @@ error_message(){
 }
 
 ###############################################################
-# Start staging
+# Start staging for gfs and gefs system
 
 # Stage the FV3 initial conditions to ROTDIR (cold start)
 YMD=${PDY} HH=${cyc} generate_com -r COM_ATMOS_INPUT
